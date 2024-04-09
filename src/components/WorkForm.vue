f56fb6a3e2d15fc80876f211ed00de3a7ec582fe
jslim <jslim9413@naver.com>
2024-04-09 16:22:20 +0900

    first commit

---
+++ src/components/WorkForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <div class="d-flex">
+            <div class="flex-grow-1 mr-2">
+                <input  type="text" 
+                        class="form-control"
+                        placeholder="new work...">
+            </div>    
+            <div>
+                <button class="btn btn-primary" type="submit">ADD</button>
+            </div> 
+        </div>
+        <!-- v-show 널값입력시 메시지 출력 -->
+        <div class="error">
+            This Field can not be empty
+        </div>
+
+    </form> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+
+// 서버기동 npm run serve
+
+// import App from './App.vue'
+// createApp(App).mount('#app')
+
+// import App from './AppSecond.vue'
+// createApp(App).mount('#app')
+
+// import App from './AppHome.vue'
+// createApp(App).mount('#app')
+
+import App from './AppIntro.vue'
+createApp(App).mount('#app')
+
+
+
